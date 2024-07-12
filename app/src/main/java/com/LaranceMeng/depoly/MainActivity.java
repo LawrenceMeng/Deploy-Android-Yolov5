4a6b871353fb97bb2678722aa66bd421975ae11f
menglei0427 <menglei0427@thundersoft.com>
2024-07-12 20:06:23 +0800

    Init

--- app/src/main/java/com/LaranceMeng/depoly/MainActivity.java
+++ app/src/main/java/com/LaranceMeng/depoly/MainActivity.java
@@ -1,8 +1,25 @@
 package com.LaranceMeng.depoly;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.camera.view.PreviewView;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Surface;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.camera.lifecycle.ProcessCameraProvider;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
